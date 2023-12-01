71ff1cf0496d7cc62a8da5f797e700f1d035213d
Olgabrezel <lumpes8524@gmail.com>
2023-12-01 08:23:31 +0100

    Day 01 Part 1

--- day01.py
+++ day01.py
@@ -1,5 +1,7 @@
 def part1(task_input):
-    pass
+    lines = task_input.split('\n')
+    digits = [list(filter(lambda x: x.isdigit(), line)) for line in lines]  # list of string of digits only for each row
+    return sum(10 * int(d[0]) + int(d[-1]) for d in digits if d)
 
 
 def part2(task_input):
