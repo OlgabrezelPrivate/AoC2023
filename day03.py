a32948bab209807dad8f3193e4404150354db5cc
Olgabrezel <lumpes8524@gmail.com>
2023-12-03 13:22:33 +0100

    Day 03 Part 2

--- day03.py
+++ day03.py
@@ -29,4 +29,25 @@ def part1(task_input):
 
 
 def part2(task_input):
-    pass
+    lines = task_input.split('\n')
+
+    ratio_sum = 0
+    for y in range(len(lines)):
+        line = lines[y]
+        for x in range(len(line)):
+            c = line[x]
+            if c == '*':
+                adjacent_num_places = set()
+                for i in range(-1, 2):
+                    if 0 <= y + i < len(lines):
+                        for j in range(-1, 2):
+                            if 0 <= x + j < len(lines[y + i]):
+                                if lines[y + i][x + j].isdigit():
+                                    adjacent_num_places.add((y + i, get_num_place(lines[y + i], x + j)))
+
+                if len(adjacent_num_places) == 2:
+                    line1, (start1, stop1) = adjacent_num_places.pop()
+                    line2, (start2, stop2) = adjacent_num_places.pop()
+                    ratio_sum += int(lines[line1][start1:stop1 + 1]) * int(lines[line2][start2:stop2 + 1])
+
+    return ratio_sum
