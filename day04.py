ce78c7824a29676c6f68064d420ebdafb125bdeb
Olgabrezel <lumpes8524@gmail.com>
2023-12-04 08:23:26 +0100

    Day 04 Part 2

--- day04.py
+++ day04.py
@@ -19,4 +19,17 @@ def part1(task_input):
 
 
 def part2(task_input):
-    pass
+    lines = task_input.split('\n')
+    card_count = {x: 1 for x in range(1, len(lines) + 1)}
+
+    for line in lines:
+        parts = line.split(': ')
+        card_no = int(parts[0][parts[0].index(' ') + 1:])
+        numbers = parts[1].split(' | ')
+        winning_nums = {int(x) for x in numbers[0].split(' ') if x}
+        have_nums = {int(x) for x in numbers[1].split(' ') if x}
+        matching = len(winning_nums.intersection(have_nums))
+        for i in range(matching):
+            card_count[card_no + 1 + i] += card_count[card_no]
+
+    return sum(card_count.values())
