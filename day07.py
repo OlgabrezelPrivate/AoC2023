3201efac9914a427500fcba2e555f6f455283138
Olgabrezel <lumpes8524@gmail.com>
2023-12-07 07:05:47 +0100

    Day 07 Part 2

--- day07.py
+++ day07.py
@@ -1,21 +1,33 @@
 from functools import total_ordering
 
 
-CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 KINDS = ['HIGH', 'PAIR', 'TWOPAIRS', 'THREE', 'FULL', 'FOUR', 'FIVE']
+CARDS = [['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'],  # Part 1
+         ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']]  # Part 2
 
 
 @total_ordering
 class Deck:
-    def __init__(self, cards):
+    def __init__(self, cards, task_part):
         self.cards = cards
+        self.task_part = task_part
         self.kind_idx = KINDS.index(self.get_kind())
 
     def __eq__(self, other):
         return type(other) is Deck and all(self.cards[i] == other.cards[i] for i in range(5))
 
     def get_kind(self):
-        counts = sorted([self.cards.count(c) for c in CARDS], reverse=True)
+        if self.task_part == 2:
+            # Gets the best card to replace jokers with. This is in any case just the card (other than a joker)
+            # that already appears the most often in the deck. Since we are only determining the kind of deck here,
+            # it doesn't matter which card the joker is replaced by in case of ties.
+            card_counts = {c: self.cards.count(c) for c in self.cards if c != 'J'} or {'A': 5}
+            best_card = next((x for x in card_counts if card_counts[x] == max(card_counts.values())))
+            cards = self.cards.replace('J', best_card)
+        else:
+            cards = self.cards
+
+        counts = sorted([cards.count(c) for c in CARDS[self.task_part - 1]], reverse=True)
         if counts[0] == 5:
             return 'FIVE'
         if counts[0] == 4:
@@ -36,8 +48,8 @@ class Deck:
         if self.kind_idx > other.kind_idx:
             return False
         for i in range(5):
-            own_card = CARDS.index(self.cards[i])
-            other_card = CARDS.index(other.cards[i])
+            own_card = CARDS[self.task_part - 1].index(self.cards[i])
+            other_card = CARDS[other.task_part - 1].index(other.cards[i])
             if own_card < other_card:
                 return True
             if other_card < own_card:
@@ -46,16 +58,15 @@ class Deck:
         return False
 
 
-def parse_input(task_input):
+def parse_input(task_input, task_part):
     decks = []
     for row in task_input.split('\n'):
         cards, bid = row.split(' ')
-        decks.append((Deck(list(cards)), int(bid)))
+        decks.append((Deck(cards, task_part), int(bid)))
     return decks
 
 
-def part1(task_input):
-    decks = parse_input(task_input)
+def get_winnings(decks):
     sorted_decks = sorted(decks, key=lambda tpl: tpl[0])
     winnings = 0
 
@@ -65,5 +76,11 @@ def part1(task_input):
     return winnings
 
 
+def part1(task_input):
+    decks = parse_input(task_input, 1)
+    return get_winnings(decks)
+
+
 def part2(task_input):
-    pass
+    decks = parse_input(task_input, 2)
+    return get_winnings(decks)
