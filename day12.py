4b42cfd98e5ec0c6f7592f5d217bcbfd1b827a95
Olgabrezel <lumpes8524@gmail.com>
2023-12-12 11:29:20 +0100

    Day 12 Part 2

--- day12.py
+++ day12.py
@@ -6,9 +6,12 @@ def parse_input(task_input):
     return springs, configurations
 
 
-def transform_springs(springs):
+def transform_springs(springs, part):
     """Removes consecutive .'s and makes sure there's a . at the beginning and the end"""
     for i in range(len(springs)):
+        if part == 2:
+            springs[i] = (springs[i] + ['?']) * 4 + springs[i]
+
         j = 0
         while j < len(springs[i]) - 1:
             if springs[i][j] == springs[i][j+1] == '.':
@@ -20,6 +23,11 @@ def transform_springs(springs):
             springs[i].insert(0, '.')
         if springs[i][-1] != '.':
             springs[i].append('.')
+
+        springs[i] = ''.join(springs[i])
+
+
+CACHE = dict()
 
 
 def get_arrangements_count(spring, conf):
@@ -33,8 +41,9 @@ def get_arrangements_count(spring, conf):
     elif ('#' not in spring) and ('?' not in spring):
         return 0
 
-    max_idx = min(spring.index('#') if '#' in spring else len(spring) - 1,
-                  len(spring) - sum(conf))
+    # First index that NEEDS to be a defect spring
+    max_idx = min(spring.index('#') if '#' in spring else (len(spring) - 1),
+                  len(spring) - sum(conf) - 1)
 
     for i in range(1, max_idx + 1):
         partspring = spring[i:]          # Remove i points from the start of the spring, at least one
@@ -42,14 +51,19 @@ def get_arrangements_count(spring, conf):
             continue
 
         # Remove the conf[0] defect segments and go on recursively
-        arrangements_count += get_arrangements_count(partspring[conf[0]:], conf[1:])
+        new_spring = partspring[conf[0]:]
+        new_conf_tpl = tuple(conf[1:])
+        if (new_spring, new_conf_tpl) not in CACHE:
+            CACHE[(new_spring, new_conf_tpl)] = get_arrangements_count(new_spring, conf[1:])
+
+        arrangements_count += CACHE[(new_spring, new_conf_tpl)]
 
     return arrangements_count
 
 
 def part1(task_input):
     springs, configurations = parse_input(task_input)
-    transform_springs(springs)
+    transform_springs(springs, 1)
     arrangements_count = 0
     for i in range(len(springs)):
         spring = springs[i]
@@ -59,4 +73,11 @@ def part1(task_input):
 
 
 def part2(task_input):
-    pass
+    springs, configurations = parse_input(task_input)
+    transform_springs(springs, 2)
+    arrangements_count = 0
+    for i in range(len(springs)):
+        spring = springs[i]
+        conf = configurations[i] * 5
+        arrangements_count += get_arrangements_count(spring, conf)
+    return arrangements_count
