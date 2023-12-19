544ad3e6a73ebd35a401c833a674937757453a43
Olgabrezel <lumpes8524@gmail.com>
2023-12-19 10:19:50 +0100

    Day 19 Part 2

--- day19.py
+++ day19.py
@@ -57,5 +57,40 @@ def part1(task_input):
     return result
 
 
+def accepted_parts_count(named_workflows, workflow_name, ranges):
+    result = 0
+    if workflow_name == 'R':
+        return 0
+    if workflow_name == 'A':
+        return ((ranges['x'][1] - ranges['x'][0] + 1) * (ranges['m'][1] - ranges['m'][0] + 1) *
+                (ranges['a'][1] - ranges['a'][0] + 1) * (ranges['s'][1] - ranges['s'][0] + 1))
+
+    workflow = named_workflows[workflow_name]
+    for ins in workflow.instructions:
+        match ins[1]:
+            case '<':
+                if ranges[ins[0]][1] < ins[2]:  # the ENTIRE range satisfies the condition - can stop the iteration here
+                    return result + accepted_parts_count(named_workflows, ins[3], ranges)
+                if ranges[ins[0]][0] < ins[2]:  # Part of the range satisfies the condition - continue iterating with rest
+                    smaller_ranges = ranges.copy()
+                    smaller_ranges[ins[0]] = (smaller_ranges[ins[0]][0], ins[2] - 1)
+                    result += accepted_parts_count(named_workflows, ins[3], smaller_ranges)
+                    ranges[ins[0]] = (ins[2], ranges[ins[0]][1])
+            case '>':
+                if ranges[ins[0]][0] > ins[2]:  # the ENTIRE range satisfies the condition - can stop the iteration here
+                    return result + accepted_parts_count(named_workflows, ins[3], ranges)
+                if ranges[ins[0]][1] > ins[2]:  # Part of the range satisfies the condition - continue iterating with rest
+                    larger_ranges = ranges.copy()
+                    larger_ranges[ins[0]] = (ins[2] + 1, larger_ranges[ins[0]][1])
+                    result += accepted_parts_count(named_workflows, ins[3], larger_ranges)
+                    ranges[ins[0]] = (ranges[ins[0]][0], ins[2])
+
+    return result + accepted_parts_count(named_workflows, workflow.else_branch, ranges)
+
+
 def part2(task_input):
-    pass
+    workflows = [Workflow(x) for x in task_input.split('\n\n')[0].split('\n')]
+    named_workflows = {x.name: x for x in workflows}
+
+    x, m, a, s = (1, 4000), (1, 4000), (1, 4000), (1, 4000)
+    return accepted_parts_count(named_workflows, 'in', {'x': x, 'm': m, 'a': a, 's': s})
